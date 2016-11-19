622a8556bdcb1859230c7e2b7c4073bf5b3d9552
Thomas Chevet <chevet.thomas@gmail.com>
2016-11-20 00:16:59 +0100

    Added function createFolder

--- creator.c
+++ creator.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void createFolder(char* folderName)
+{
+	char *commandBuffer = (char*) malloc(262*sizeof(char));
+	
+	sprintf(commandBuffer, "mkdir %s", folderName);
+	system(commandBuffer);
+
+	free(commandBuffer);
+}
+
 void createProject()
 {
 	char *folderName = (char*) malloc(256*sizeof(char));
-	char *commandBuffer = (char*) malloc(512*sizeof(char));
 
 	printf(
 		"You are going to create a new LaTeX project\n"
 		"Enter a name for the project: ");
 	scanf("%s", folderName);
 
-	sprintf(commandBuffer, "mkdir %s", folderName);
-	printf("%s", commandBuffer);
-	system(commandBuffer);
+	createFolder(folderName);
 
-	free(commandBuffer);
 	free(folderName);
 }
 
