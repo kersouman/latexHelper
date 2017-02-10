e93e4d89aba9f0c09a1fd693f249a6c4b72d3182
Thomas Chevet <chevet.thomas@gmail.com>
2017-02-10 23:29:13 +0100

    Added dispatchers

--- creator.py
+++ creator.py
@@ -1,7 +1,23 @@
+import sys
 import os
+
+def dispatcher(argv):
+    try:
+        toCreate = argv[0]
+    except IndexError:
+        print("Latexhelper: No new type specified\n" + \
+            "Try\n" + \
+            "\tlatexhelper help\n" + \
+            "to get usage information")
+        sys.exit()
+    if toCreate in ("-p", "project"):
+        createProject()
 
 def createProject():
     print("You are going to create a new LaTeX project")
-    project = input("Enter a name for the project: ")
+    
+    project = os.getcwd()
+    project += "/" + input("Enter a name for the project: ")
+
     if not os.path.exists(project):
         os.makedirs(project)
