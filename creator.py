e33b667c49e00af0fe2da6318196390bc53e8b22
Thomas Chevet <chevet.thomas@gmail.com>
2017-02-11 22:19:36 +0100

    Addition of some tex files

--- creator.py
+++ creator.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import shutil
+import sys
 
 import messages
 
@@ -9,16 +10,30 @@ def dispatcher(argv):
     except IndexError:
         messages.helpMessage("No new type specified")
         sys.exit()
+
     if toCreate in ("-p", "project"):
         createProject()
+
+
 
 def createProject():
     print("You are going to create a new LaTeX project")
     projectName = input("Enter a name for the project: ")
 
     projectPath = os.getcwd() + "/" + projectName
+    cacheFolder = projectPath + "/.latexhelper"
+    preambleFolder = projectPath + "/preamble"
+    preambleFile = projectPath + "/preamble.tex"
+
+    softPath = os.path.dirname(__main__)
+    originPreamblePath = softPath + "/latex_files/preamble.tex"
+    originPreambleFolderPath = softPath + "/latex_files/preamble"
 
     if not os.path.exists(projectPath):
         os.makedirs(projectPath)
+        os.makedirs(cacheFolder)
+        shutil.copyfile(originPreamblePath, preambleFile)
+        shutil.copy(originPreambleFolderPath, preambleFolder)
     else:
         print("The project " + projectName + " already exists")
+
